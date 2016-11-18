a71c3783dec6e502281fe5ceeb8683c771348452
ParkBeomMin <club20608@gmail.com>
2016-11-19 04:40:07 +0900

    week3

---
+++ str.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mystrlen(char*);
+
+void mystrcpy(char*,char*);
+
+void mystrncpy(char*, char*, int);
+
+void mystrcat(char*, char*);
+
+int mystrcmp(char*, char*);
+
+char* mystrchr(char*, char);
+
+void print_str(char*, char*);
+void print_strlen(char*);
+void print_strcpy(char*, char*);
+void print_strncpy(char*, char*, int);
+void print_strcat(char*, char*);
+void print_strcmp(char*, char*);
+void print_strchr(char*, char);

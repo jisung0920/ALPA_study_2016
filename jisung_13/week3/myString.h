d7d8042d07925f28c934cd319a6e2861b42fde1d
jisung0920 <jisung0920@gmail.com>
2016-11-19 22:32:17 +0900

    header

---
+++ jisung_13/week3/myString.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int mStrlen(char*);           // length of string
+void mStrcpy(char, char);     // copy string
+void mStrncpy(char,char,int); // copy string
+void strcat(char*,char*);     // add string
+int strcmp(char*,char*);      // compare string by dic order
+int strchr(char*,char);       // char searching

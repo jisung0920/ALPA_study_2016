9b5c9c3f2100aaed07d96c7b3b6274357c8e6b58
jisung0920 <jisung0920@gmail.com>
2016-11-22 23:04:35 +0900

    code

--- jisung_13/week3/mString.c
+++ jisung_13/week3/mString.c
@@ -10,31 +10,34 @@ int mStrlen(const char* str){
   return cnt-1;
 }
 
-void mStrcpy(const char *str1,char *str2){
+char* mStrcpy(const char *str1,char *str2){
   int l,i;
   l=mStrlen(str1);
   for(i=0;i<l;i++)
       *(str2+i)=*(str1+i);
   *(str2+i)='\0';
+  return str1;
 }     // copy string
 
-void mStrncpy(const char *str1,char *str2,int n){
+char* mStrncpy(const char *str1,char *str2,int n){
   int i,l;
   l=mStrlen(str1);
   if(n>l)
     n=l;
-  for(i=0;i<n;i++)
+  for(i=0;i<n-1;i++)
       *(str2+i)=*(str1+i);
   *(str2+i)='\0';
+  return str1;
 }
 
-void mStrcat(char *str1,const char *str2){
+char* mStrcat(char *str1,const char *str2){
   int l1,l2,i,j=0;
   l1=mStrlen(str1);
   l2=mStrlen(str2);
   for(i=l1;i<(l1+l2);i++)
       *(str1+i)=*(str2+j++);
     *(str1+i)='\0';
+  return str1;
 }     // add string
 int mStrcmp(const char *str1,const char *str2){
   int l1,l2,i;
